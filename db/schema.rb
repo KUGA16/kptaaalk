44d5b4e2e727409acdeb350b35ab0191e253cd94
KUGA16 <gachan_1216@yahoo.co.jp>
2020-02-23 08:36:29 +0000

    Add: グループモデル、カラムを作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_21_165246) do
+ActiveRecord::Schema.define(version: 2020_02_23_083414) do
+
+  create_table "groups", force: :cascade do |t|
+    t.string "name"
+    t.string "group_image_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
