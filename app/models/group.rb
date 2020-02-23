6cfa1b5287811baa31402066884d4e266be19d0c
KUGA16 <gachan_1216@yahoo.co.jp>
2020-02-23 10:59:48 +0000

    Add: 画像を挿入できるようにattachmentを定義

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,7 +1,8 @@
 class Group < ApplicationRecord
   #グループ名:空白禁止、1文字以上20文字以内
   validates :name,presence: true,length: { in: 1..20 }
-
+  #画像を挿入するため
+  attachment :group_image
   #groupが消えるとgroup_userも消える
   has_many :group_users, dependent: :destroy
 end
