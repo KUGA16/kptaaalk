5ac55e3d5ad44189386847f5cfa2120c919216ed
KUGA16 <gachan_1216@yahoo.co.jp>
2020-02-23 09:20:59 +0000

    Add: グループ名に対してバリデーションを追加

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,4 +1,7 @@
 class Group < ApplicationRecord
+  #グループ名:空白禁止、1文字以上20文字以内
+  validates :name,presence: true,length: { in: 1..20 }
+
   #groupが消えるとgroup_userも消える
   has_many :group_users, dependent: :destroy
 end
