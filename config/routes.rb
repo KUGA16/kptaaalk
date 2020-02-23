d208b81202fa2f1f643569916cff63460e536455
KUGA16 <gachan_1216@yahoo.co.jp>
2020-02-23 12:36:12 +0000

    Add: グループユーザーのコントローラーを作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'group_users/new'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   #deviseを使用する際にURLとしてusersを含む
   devise_for :users
