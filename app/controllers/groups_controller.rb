066f682e9d63dc1cb0848e8dc436c4c8632ef233
KUGA16 <gachan_1216@yahoo.co.jp>
2020-02-23 10:12:49 +0000

    Add: groupsコントローラー追加、アクション定義

---
+++ app/controllers/groups_controller.rb
@@ -0,0 +1,18 @@
+class GroupsController < ApplicationController
+
+  def show
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def destroy
+  end
+  
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get 'groups/show'
+  get 'groups/new'
+  get 'groups/edit'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   #deviseを使用する際にURLとしてusersを含む
   devise_for :users
