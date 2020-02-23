7f3370ac356113178df199226c142d9d46788a3a
KUGA16 <gachan_1216@yahoo.co.jp>
2020-02-23 10:40:00 +0000

    Add: グループコントローラーのルーティングを作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,15 @@
 Rails.application.routes.draw do
-  get 'groups/show'
-  get 'groups/new'
-  get 'groups/edit'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   #deviseを使用する際にURLとしてusersを含む
   devise_for :users
 
   root 'homes#top'
   get 'about' => 'homes#about', as: 'about'
-
   resources :users, only: [:show, :edit, :update, :destroy]do
     collection do
       get 'result'
     end
   end
-  
+  resources :groups, only: [:show, :new, :create, :edit, :update, :destroy]
+
 end
