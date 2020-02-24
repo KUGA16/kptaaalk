f618d32d5f0086a7edde1075d9382f70eae5ec09
KUGA16 <gachan_1216@yahoo.co.jp>
2020-02-24 06:03:48 +0000

    Add: グループ名を一意にする為にバリデーションを追加

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,6 +1,6 @@
 class Group < ApplicationRecord
-  #グループ名:空白禁止、1文字以上20文字以内
-  validates :name,presence: true,length: { in: 1..20 }
+  #グループ名:空白禁止、、一意であること、1文字以上20文字以内
+  validates :name, presence: true, uniqueness: true, length: { in: 1..20 }
   #画像を挿入するため
   attachment :group_image
   #groupが消えるとgroup_userも消える
