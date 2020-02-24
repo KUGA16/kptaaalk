32e3494efc0da30ca9e28f69a50d3be46637ddca
KUGA16 <gachan_1216@yahoo.co.jp>
2020-02-24 10:15:05 +0000

    Change: リファクタリング

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,10 +1,9 @@
 class Group < ApplicationRecord
+  has_many :group_users, dependent: :destroy #groupが消えるとgroup_userも消える
+  has_many :comments,    dependent: :destroy #groupが消えるとcommentも消える
+
+  attachment :group_image#画像を挿入するため
   #グループ名:空白禁止、、一意であること、1文字以上20文字以内
   validates :name, presence: true, uniqueness: true, length: { in: 1..20 }
-  #画像を挿入するため
-  attachment :group_image
-  #groupが消えるとgroup_userも消える
-  has_many :group_users, dependent: :destroy
-  #groupが消えるとcommentも消える
-  has_many :comments, dependent: :destroy
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,14 +3,12 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  #refileが指定のカラムにアクセスするために必要
-  attachment :profile_image
-  #名前:空白禁止
-  validates :name,presence: true
-  #ニックネーム:空白禁止、一意である
-  validates :nick_name, presence: true, uniqueness: true
-  #userが消えるとgroup_userも消える
-  has_many :group_users, dependent: :destroy
-  #userが消えるとcommentも消える
-  has_many :comments, dependent: :destroy
+
+  has_many :group_users, dependent: :destroy#userが消えるとgroup_userも消える
+  has_many :comments,    dependent: :destroy #userが消えるとcommentも消える
+  attachment :profile_image #refileが指定のカラムにアクセスするために必要
+
+  validates :name,      presence: true #名前:空白禁止
+  validates :nick_name, presence: true, uniqueness: true #ニックネーム:空白禁止、一意である
+
 end
