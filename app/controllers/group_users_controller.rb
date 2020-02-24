d47e95699590bacaba3eebaeb479dd709106ab45
KUGA16 <gachan_1216@yahoo.co.jp>
2020-02-24 09:53:17 +0000

    Add: createアクションにコメントアウト追加

--- app/controllers/group_users_controller.rb
+++ app/controllers/group_users_controller.rb
@@ -4,9 +4,19 @@ class GroupUsersController < ApplicationController
     #ログインユーザー以外のユーザー
     @users = User.where.not(id: current_user.id)
     @group = Group.find(params[:id])
+    @group_user_new = GroupUser.new
   end
 
   def create
+    if @group_user_new = GroupUser.new(group_users_params)
+       # @group_user_new.user_id = current_user.id
+       # @group_user_new.grop_id = #自分が作ったグループ
+       # @group_user_new.is_confirmed =
+       # @group_user_new.save!
+       # redirect_to comments_path
+    else
+      # render 'new'
+    end
   end
 
 end
