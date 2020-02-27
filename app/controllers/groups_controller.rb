ce0beadbb72f24a254bab7751c6337f3b71604a4
KUGA16 <gachan_1216@yahoo.co.jp>
2020-02-27 05:53:28 +0000

    Add: グループ作成時、作成者のidと作成したグループのidを入れたレコードを作成

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -8,13 +8,12 @@ class GroupsController < ApplicationController
   end
 
   def create
-    if  @group_new = Group.create(group_params)
-        @group_user_new = GroupUser.new(
+    if  @group_new = Group.create!(group_params)
+        GroupUser.create!(
           user_id: current_user.id,
           group_id: @group_new.id,
           is_confirmed: true
         )
-        @group_user_new.save
         redirect_to new_group_group_users_path(@group_new)
     else
         render 'new'
--- app/models/group_user.rb
+++ app/models/group_user.rb
@@ -1,4 +1,4 @@
-class GroupUsers < ApplicationRecord
+class GroupUser < ApplicationRecord
   belongs_to :group
   belongs_to :user
 
