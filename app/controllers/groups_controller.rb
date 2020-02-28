3ddb22ab7a971df7dc0794f05ba1626373693026
KUGA16 <gachan_1216@yahoo.co.jp>
2020-02-28 20:23:28 +0000

    Change: update後のリダイレクト先を変更

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -31,7 +31,7 @@ class GroupsController < ApplicationController
   def update
     # @group = Group.find(params[:id])
     if  @group.update(group_params)
-        redirect_to group_comments_path(@group)
+        redirect_to group_path(@group)
     else
         render 'edit'
     end
