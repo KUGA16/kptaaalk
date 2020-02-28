928a5ee419708360240bad8f75936d5bb327ac82
KUGA16 <gachan_1216@yahoo.co.jp>
2020-02-28 18:46:49 +0000

    Add: グループ画像と名前をクリック時にグループ詳細画面へ飛ぶ

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,7 +1,7 @@
 class Group < ApplicationRecord
   has_many :users, through: :group_users
-  has_many :group_users,  dependent: :destroy #groupが消えるとmemberも消える
-  has_many :comments, dependent: :destroy #groupが消えるとcommentも消える
+  has_many :group_users, dependent: :destroy #groupが消えるとgroup_userも消える
+  has_many :comments, dependent: :destroy #groupが消えるとgroup_userも消える
 
   attachment :group_image#画像を挿入するため
   #グループ名:空白禁止、、一意であること、1文字以上20文字以内
