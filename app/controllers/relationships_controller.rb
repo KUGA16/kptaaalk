6745ec380b6b99b6a6b2c58c4f1ab76b6416182b
KUGA16 <gachan_1216@yahoo.co.jp>
2020-02-29 10:55:57 +0000

    Change: フォローした後のリダイレクトを今の画面のままにした

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -2,12 +2,12 @@ class RelationshipsController < ApplicationController
   def create
     follow = current_user.active_relationships.build(follower_id: params[:user_id])
     follow.save
-    redirect_to result_users_path
+    redirect_back(fallback_location: result_users_path)
   end
 
   def destroy
     follow = current_user.active_relationships.find_by(follower_id: params[:user_id])
     follow.destroy
-    redirect_to result_users_path
+    redirect_back(fallback_location: result_users_path)
   end
 end
