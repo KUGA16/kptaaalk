edf0afd3b4e5780e96e5960ee584c77068ae7eba
KUGA16 <gachan_1216@yahoo.co.jp>
2020-02-29 11:56:20 +0000

    Add: リファクタリング

--- app/controllers/group_users_controller.rb
+++ app/controllers/group_users_controller.rb
@@ -11,12 +11,10 @@ class GroupUsersController < ApplicationController
 
   def create
     group = Group.find(params[:group_id])
-    # group_user_new = GroupUser.new(group_user_params)
     params[:group_user][:user_id].each do |id|
       if GroupUser.create!(user_id: id, group_id: params[:group_id])
          redirect_to group_comments_path(group)
-      else
-      # render 'new'
+         return
       end
     end
   end
