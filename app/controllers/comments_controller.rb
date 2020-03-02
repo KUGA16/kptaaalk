7cd37f00b255eab96b48475559dd0b0fee7bdd18
KUGA16 <gachan_1216@yahoo.co.jp>
2020-03-02 08:59:30 +0000

    Add: keep,probrem,tryを別々に表示する為にplace_status別で変数に定義

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -8,7 +8,10 @@ class CommentsController < ApplicationController
     @group_users = GroupUser.where(group_id: params_group_id).where(is_confirmed: true)
     #このグループのみのコメント
     @comments = Comment.where(group_id: params_group_id)
-    Comment.where(group_id: params_group_id).group_by(&:place_status)
+    # place_statusでKPTを分けて取得
+    @comments_place_keep = Comment.where(group_id: params_group_id, place_status: "keep")
+    @comments_place_probrem = Comment.where(group_id: params_group_id, place_status: "probrem")
+    @comments_place_try = Comment.where(group_id: params_group_id, place_status: "try")
   end
 
   def new
