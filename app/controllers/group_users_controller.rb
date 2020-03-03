d3b9d6d2ff0cb9d713615dee668e99bb0a17aff7
KUGA16 <gachan_1216@yahoo.co.jp>
2020-03-03 11:31:32 +0000

    Add: グループ招待を断れる機能を追加

--- app/controllers/group_users_controller.rb
+++ app/controllers/group_users_controller.rb
@@ -3,31 +3,31 @@ class GroupUsersController < ApplicationController
   before_action :authenticate_user!
 
   def new
-    @group = Group.find(params[:group_id])
-    @follower_ids = current_user.followings.pluck(:id) #フォローしているユーザーを配列で取得
-    #params[:group_id]と同じgroup_idのGroupUserのuser_idを配列かつint型で取得
-    @group_user_ids = GroupUser.where(group_id: params[:group_id]).pluck(:user_id).map!(&:to_i)
-    @invite_user_ids = @follower_ids - @group_user_ids #差分を取得
-    @can_invite_users = User.find(@invite_user_ids) #差分user_idを持つuserの情報を取得
-    @join_users = GroupUser.where(group_id: params[:group_id]).where(is_confirmed: true)
     @group_user_new = GroupUser.new
+    @group = Group.find(params[:group_id])
+    follower_ids = current_user.followings.pluck(:id) #フォローしているユーザーを配列で取得
+    #params[:group_id]と同じgroup_idのGroupUserのuser_idを配列かつint型で取得
+    group_user_ids = GroupUser.where(group_id: params[:group_id]).pluck(:user_id).map!(&:to_i)
+    invite_user_ids = follower_ids - group_user_ids #差分を取得
+    @can_invite_users = User.find(invite_user_ids) #差分user_idを持つuserの情報を取得
+    @join_users = GroupUser.where(group_id: params[:group_id]).where(is_confirmed: true)
   end
 
   def create
-    @group = Group.find(params[:group_id])
+    group = Group.find(params[:group_id])
     if params[:group_user].blank? #ユーザーを未選択でsubmitした時
-      redirect_to new_group_group_users_path(@group)
+      redirect_to new_group_group_users_path(group)
       return
     end
     begin #通常時
       params_group_user_ids[:user_id].each do |id|
-        GroupUser.create!(user_id: id, group_id: @group.id)
+        GroupUser.create!(user_id: id, group_id: group.id)
       end
     rescue => e #エラー時
-      redirect_to new_group_group_users_path(@group)
+      redirect_to new_group_group_users_path(group)
       return
     end
-      redirect_to group_comments_path(@group)
+      redirect_to group_comments_path(group)
   end
 
   def update
@@ -42,8 +42,9 @@ class GroupUsersController < ApplicationController
   end
 
   def destroy
-    no_join_group = GroupUser.find(params[:group_id])
-    binding.pry
+    no_join_group = GroupUser.find_by(group_id: params[:group_id], user_id: current_user)
+    no_join_group.destroy
+    redirect_to user_path(current_user)
   end
 
 
