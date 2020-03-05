e31a571a8fba3ffd339a0845e1dbd54df45ec3a5
KUGA16 <gachan_1216@yahoo.co.jp>
2020-03-05 09:25:06 +0000

    Change: フォロー機能非同期（未完成）

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -1,13 +1,13 @@
 class RelationshipsController < ApplicationController
   def create
-    follow = current_user.active_relationships.build(follower_id: params[:user_id])
-    follow.save
+    @users = current_user.active_relationships.build(follower_id: params[:user_id])
+    @users.save
     redirect_back(fallback_location: result_users_path)
   end
 
   def destroy
-    follow = current_user.active_relationships.find_by(follower_id: params[:user_id])
-    follow.destroy
+    @users = current_user.active_relationships.find_by(follower_id: params[:user_id])
+    @users.destroy
     redirect_back(fallback_location: result_users_path)
   end
 end
