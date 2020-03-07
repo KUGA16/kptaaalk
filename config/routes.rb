e524a5fe9fd3eedf83cf4bea3ee91d025139efac
KUGA16 <gachan_1216@yahoo.co.jp>
2020-03-07 15:28:39 +0000

    Change: aboutページを無くしてtopページと一緒にした

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,6 @@ Rails.application.routes.draw do
   devise_for :users
 
   root 'homes#top'
-  get 'about' => 'homes#about', as: 'about'
 
   resources :users,          only: [:show, :edit, :update, :destroy] do
     resource :relationships, only: [:create,:destroy]
