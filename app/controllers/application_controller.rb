12ce7bd6dfb72537d70eddf748b405ee42cc7aee
KUGA16 <gachan_1216@yahoo.co.jp>
2020-03-08 08:06:30 +0000

    Change: モデルにグループから招待されているかのメソッドを追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,22 +3,22 @@ class ApplicationController < ActionController::Base
   #devise利用の機能（ユーザ登録、ログイン認証など）の前に実行
   before_action :configure_permitted_parameters, if: :devise_controller?
   before_action :is_notification
-
-  def after_sign_in_path_for(resource) #ログイン後の画面遷移
+ #ログイン後の画面遷移
+  def after_sign_in_path_for(resource)
     user_path(current_user)
   end
-
-  def after_sign_out_path_for(resource) #ログアウト後の画面遷移
+ #ログアウト後の画面遷移
+  def after_sign_out_path_for(resource)
     root_path
   end
-
+# ログインユーザーがグループに招待されているか
   def is_notification
-    if current_user.present?
-      @is_notification = GroupUser.where(user_id: current_user.id).where(is_confirmed: false)
+    if user_signed_in?
+      @is_notification = GroupUser.is_notification?(current_user)
     end
   end
 
-  #他のコントローラからも参照可能
+#他のコントローラからも参照可能
   protected
   #sign_up,sign_in,account_updateの際に、keyのデータ操作を許可
   def configure_permitted_parameters
