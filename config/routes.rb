c25e85a016032f01eb4fec14cc61d7a6f4a891cf
KUGA16 <gachan_1216@yahoo.co.jp>
2020-03-08 19:15:25 +0000

    Change: ルーティングを整頓

--- config/routes.rb
+++ config/routes.rb
@@ -7,8 +7,7 @@ Rails.application.routes.draw do
 
   resources :users,          only: [:show, :edit, :update, :destroy] do
     resource :relationships, only: [:create,:destroy]
-    get :follows #フォロー一覧
-    get :followers #フォロワー一覧
+    get :follows #フォロー、フォロワー一覧
     member do
       get 'withdraw' #サイト退会ページ
     end
