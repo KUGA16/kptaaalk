09b527df4880c74a7db1e53e379e6aad1e6751d2
KUGA16 <gachan_1216@yahoo.co.jp>
2020-03-08 11:48:43 +0000

    Test: ドロップしたらアイテムが一番上に来るように修正中

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -63,15 +63,42 @@ $( document ).ready(function() {
         })
         // Ajaxリクエストが成功した時
         .done((data) => {
-          console.log(data);
+  console.log(data);
+            let id = parseInt(data.id);
+            let user_id = parseInt(data.user_id);
+            let nick_name = data.nick_name;
+            let comment = data.comment;
+            let group_id = parseInt(data.group_id);
+            let user_image = data.user_image; // imageの受け取り方不明
+            let place_status = data.place_status;
+            let right = data.right;
+            changeDom(id,user_id,nick_name,comment,group_id,user_image,place_status,right);
         })
         // Ajaxリクエストが失敗した時
         .fail((data) => {
-          window.alert('正しい結果を得られませんでした。');
+          window.alert('エラーが発生しました。');
         });
         // Ajaxリクエストが成功・失敗どちらでも発動
         // .always(function(data) => {
         // });
+        // DOM操作用のfunction
+        function changeDom(id,user_id,nick_name,comment,group_id,user_image,place_status,right){
+          $("#" + place_status).find('.item').each( function( index, element ) {
+            //console.log( element );
+            $(element).before('<div class=“col s4 kpt_comments item ui-draggable ui-draggable-handle” name=“41" style=“position: relative;“>あいりKのコメント２<i class=“material-icons” style=“color: red”>thumb_up</i><span name=“3">3それな</span></a></div>')
+            return false;
+//console.log( $(element).find(".rights_count"));
+            })
+          // 挿入先判定のため要素分だけループ処理
+          // $('#tbodysamaya tr').each(function(index,element){
+          //     if(parseInt($(element).attr('id'))<= age){
+                  // 取得したelementのid(age)が登録したAgeより小さい場合にその手前に登録したuserのhtmlを挿入する。
+                  // $(element).before('<td>'+name+'</td><td>'+age+'</td><td>'+name+'</td>');
+                  // 処理完了のためループ処理を抜ける
+                  // return false;
+              // }
+          // });
+      }
     }
   });
 });
