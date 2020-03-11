a04025041e1613a7240b0ce8407d9c99f48b8fdc
KUGA16 <gachan_1216@yahoo.co.jp>
2020-03-11 05:12:10 +0000

    Add: ユーザー検索からのフォロー機能をajaxで実装

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -1,13 +1,13 @@
 class RelationshipsController < ApplicationController
   def create
-    @users = current_user.active_relationships.build(follower_id: params[:user_id])
-    @users.save
-    redirect_back(fallback_location: result_users_path)
+    @user = User.find(params[:user_id])
+    following_user = current_user.active_relationships.build(follower_id: @user.id)
+    following_user.save
   end
 
   def destroy
-    @users = current_user.active_relationships.find_by(follower_id: params[:user_id])
-    @users.destroy
-    redirect_back(fallback_location: result_users_path)
+    @user = User.find(params[:user_id])
+    follower_user = current_user.active_relationships.find_by(follower_id: @user.id)
+    follower_user.destroy
   end
 end
