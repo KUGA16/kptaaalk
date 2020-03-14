5063151742b4222cfa90f3c17785ee12374af159
KUGA16 <gachan_1216@yahoo.co.jp>
2020-03-14 07:44:01 +0000

    Fix: グループ画像カラムのデータ型を変更　

---
+++ db/migrate/20200314072448_change_groups_column.rb
@@ -0,0 +1,5 @@
+class ChangeGroupsColumn < ActiveRecord::Migration[5.2]
+  def change
+    change_column :groups, :group_image_id, :string
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_10_090252) do
+ActiveRecord::Schema.define(version: 2020_03_14_072448) do
 
   create_table "comments", force: :cascade do |t|
     t.integer "user_id"
@@ -33,7 +33,7 @@ ActiveRecord::Schema.define(version: 2020_03_10_090252) do
 
   create_table "groups", force: :cascade do |t|
     t.string "name"
-    t.integer "group_image_id"
+    t.string "group_image_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
