31b5b4f77db7ac999c0a3771c66a483338d768e8
KUGA16 <gachan_1216@yahoo.co.jp>
2020-03-21 15:47:00 +0000

    Change: コード修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,18 +9,20 @@ class User < ApplicationRecord
   has_many :comments,    dependent: :destroy #userが消えるとcommentも消える
   has_many :rights,      dependent: :destroy #userが消えるとrightも消える
 
-  # ====================自分がフォローしているユーザーとの関連 ===================================
+  # ====================自分がフォローしているユーザーとの関連 ================================
   has_many :active_relationships, class_name: "Relationship", foreign_key: :following_id
   has_many :followings, through: :active_relationships, source: :follower
-  # ========================================================================================
-  # ====================自分がフォローされるユーザーとの関連 ===================================
+  # =====================================================================================
+  # ====================自分がフォローされるユーザーとの関連 ==================================
   has_many :passive_relationships, class_name: "Relationship", foreign_key: :follower_id
   has_many :followers, through: :passive_relationships, source: :following
-  # =======================================================================================
+  # =====================================================================================
 
   attachment :profile_image #refileが指定のカラムにアクセスするために必要
 
-  validates :nick_name, presence: true, uniqueness: true #ニックネーム:空白禁止、一意である
+  validates :nick_name,
+    presence: true,
+    uniqueness: true #ニックネーム:空白禁止、一意である
 
   def followed_by?(user)
     # フォローしようとしているユーザーがフォローされているユーザーの中から、自分がいるかどうかを調べる
