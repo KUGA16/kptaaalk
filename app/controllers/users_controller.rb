c4dccb9a9618ea3ad14b9d13c8d300bf92ce44d3
KUGA16 <gachan_1216@yahoo.co.jp>
2020-03-27 06:34:54 +0000

    Add: プロフィール画像を削除できるように実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -30,7 +30,7 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:name, :nick_name, :introduction, :profile_image)
+    params.require(:user).permit(:name, :nick_name, :introduction, :profile_image, :remove_profile_image)
   end
 
   #url直接入力禁止
