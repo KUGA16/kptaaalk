f74d98f3ee8856c0ee2c559ce8600b5f16f48521
KUGA16 <gachan_1216@yahoo.co.jp>
2020-04-02 14:58:06 +0000

    Change: js.erbファイルを一つにまとめた

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -3,11 +3,13 @@ class RelationshipsController < ApplicationController
     @user = User.find(params[:user_id])
     following_user = current_user.active_relationships.build(follower_id: @user.id)
     following_user.save
+    render 'follow'
   end
 
   def destroy
     @user = User.find(params[:user_id])
     follower_user = current_user.active_relationships.find_by(follower_id: @user.id)
     follower_user.destroy
+    render 'follow'
   end
 end
