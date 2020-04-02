d9b889cf12387e467721ed7f8e65d1f4e6f3e0e8
KUGA16 <gachan_1216@yahoo.co.jp>
2020-04-02 13:34:35 +0000

    Clean: インデント修正

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,8 +1,8 @@
 class Group < ApplicationRecord
   has_many :group_users, dependent: :destroy #groupが消えるとgroup_userも消える
-  has_many :users, through: :group_users
+  has_many :users,         through: :group_users
   has_many :comments,    dependent: :destroy #groupが消えるとcommentも消える
-  has_many :rights,       dependent: :destroy #groupが消えるとrightも消える
+  has_many :rights,      dependent: :destroy #groupが消えるとrightも消える
 
   attachment :group_image#画像を挿入するため
   #グループ名:空白禁止、一意であること、1文字以上20文字以内
