f10bd5398e53432515328244c1ef84edbfa8ab24
KUGA16 <gachan_1216@yahoo.co.jp>
2020-05-11 12:30:27 +0000

    Change: 管理者画面を自分で作成するためrails-admin削除

--- config/routes.rb
+++ config/routes.rb
@@ -1,20 +1,19 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  #homes
-  root 'homes#top'
-
   #users
   #deviseを使用する際にURLとしてusersを含む
   devise_for :users, :controllers => {
     :registrations => 'users/registrations'
   }
+
+  root 'homes#top'
+  
   resources :users, only: [:show, :edit, :update], param: :user_id
   resources :users, only: [] do
     resource :relationships, only: [:create, :destroy]
     get 'friends' #フォロー、フォロワー一覧
   end
-
   #groups
   resources :groups, only: [:show, :new, :create, :edit, :update, :destroy], param: :group_id
   resources :groups, only: [] do
@@ -25,14 +24,9 @@ Rails.application.routes.draw do
       resource :rights,    only: [:create, :destroy]
     end
   end
-
   #commnets > ドラッグ&ドロップ
   post 'place_status_update' => 'comments#place_status_update'
-
   #search
   get 'search' => 'search#users', as: 'search_users'
-
-  # admin
-  mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
 
 end
