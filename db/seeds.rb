803d1ea3a7ad8e8985b8411ade783bbba47cc235
KUGA16 <gachan_1216@yahoo.co.jp>
2020-06-19 09:25:40 +0000

    Add: とりあえず簡単な初期データを追加、まだ完成ではないgit status!

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,44 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+# テストユーザー
+User.create!(nick_name: "KPT太郎", email: "kpt@kpt", password: "kptlove")
+
+50.times do |i|
+
+  user = User.create!(
+    nick_name: "TEST-#{i}",
+    email: "kpt@#{i}",
+    password: "password",
+    introduction: "テストユーザーです。"
+  )
+
+  group = Group.create!(
+    name: "TEST-GROUP-#{i}"
+  )
+
+  if i % 3 == 0
+    is_confirmed = true
+  else
+    is_confirmed = false
+  end
+
+  GroupUser.create!(
+    user_id: user.id,
+    group_id: group.id,
+    is_confirmed: is_confirmed
+  )
+
+  if i % 3 == 0
+    place_status = 0
+  elsif i % 5 == 0
+    place_status = 1
+  else
+    place_status = 2
+  end
+
+  Comment.create!(
+    user_id: user.id,
+    group_id: group.id,
+    comment: "テストコメント#{i}",
+    place_status: place_status
+  )
+
+end
