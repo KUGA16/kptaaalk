370db3907b4c2c366f2735588f452b2c8b997045
KUGA16 <gachan_1216@yahoo.co.jp>
2020-06-19 07:54:07 +0000

    Add: グループ一覧にグループ検索機能実装

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -12,4 +12,29 @@ class SearchController < ApplicationController
     @search_users = @q.result(distinct: true).where.not(id: current_user.id)
   end
 
+  def groups
+    @user = current_user
+    @joined_groups = []
+    @invited_groups = []
+    @q = Group.ransack(params[:q])
+    groups = @q.result(distinct: true)
+    # groupsは当てハマるDB全てのgroup
+    groups.each do |group|
+      group.group_users.each do |group_user|
+        # group_user.user_idがcurrent_userであることと
+        if group_user.user_id == current_user.id
+          # is_confirmedが参加、招待中によって変数を変更
+          case group_user.is_confirmed
+          when true
+            @joined_groups << group_user
+          when false
+            @invited_groups << group_user
+          end
+        end
+        # group_user.user_idがcurrent_userでない時は[]で返す
+      end
+    end
+    render "users/show"
+  end
+
 end
